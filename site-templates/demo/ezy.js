c1a4711f34d6d6e29da05a855920c32a22fc706a
norma-studio <65451081+norma-studio@users.noreply.github.com>
2020-06-15 17:44:19 +0400

    Add files via upload

---
+++ site-templates/demo/ezy.js
@@ -0,0 +1,67 @@
+var _href2 = window.location.hash.slice(1);
+// var arr = new Map([
+//   ['101000', 'http://avehtml.liquid-themes.com/index.html'], 
+
+// ]);
+
+var articles101 = {
+"203201":[{"id":"203201", "btn":"002", "name":"Ezy Multi Purpose 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-1.html"}],
+"203202":[{"id":"203202", "btn":"002", "name":"Ezy Multi Purpose 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-2.html"}],
+"203203":[{"id":"203203", "btn":"002", "name":"Ezy Multi Purpose 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-3.html"}],
+"203204":[{"id":"203204", "btn":"002", "name":"Ezy Multi Purpose 4", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-4.html"}],
+"203205":[{"id":"203205", "btn":"002", "name":"Ezy Multi Purpose 5", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-5.html"}],
+"203206":[{"id":"203206", "btn":"002", "name":"Ezy Multi Purpose 6", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-6.html"}],
+"203207":[{"id":"203207", "btn":"002", "name":"Ezy Multi Purpose 7", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-7.html"}],
+"203208":[{"id":"203208", "btn":"002", "name":"Ezy Multi Purpose 8", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-8.html"}],
+"203209":[{"id":"203209", "btn":"002", "name":"Ezy Multi Purpose 9", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-9.html"}],
+"203210":[{"id":"203210", "btn":"002", "name":"Ezy Multi Purpose 10", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-10.html"}],
+"203211":[{"id":"203211", "btn":"002", "name":"Ezy Multi Purpose 11", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-11.html"}],
+"203212":[{"id":"203212", "btn":"002", "name":"Ezy Multi Purpose 12", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-12.html"}],
+"203213":[{"id":"203213", "btn":"002", "name":"Ezy Multi Purpose 13", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-13.html"}],
+"203214":[{"id":"203214", "btn":"002", "name":"Ezy Multi Purpose 14", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-14.html"}],
+"203215":[{"id":"203215", "btn":"002", "name":"Ezy Multi Purpose 15", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-15.html"}],
+"203216":[{"id":"203216", "btn":"002", "name":"Ezy Multi Purpose 16", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-16.html"}],
+"203217":[{"id":"203217", "btn":"002", "name":"Ezy Multi Purpose 17", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-17.html"}],
+"203218":[{"id":"203218", "btn":"002", "name":"Ezy Multi Purpose 18", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-18.html"}],
+"203219":[{"id":"203219", "btn":"002", "name":"Ezy Multi Purpose 19", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-19.html"}],
+"203220":[{"id":"203220", "btn":"002", "name":"Ezy Multi Purpose 20", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/multi-purpose-20.html"}],
+"203221":[{"id":"203221", "btn":"002", "name":"Ezy Portfolio 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-1.html"}],
+"203222":[{"id":"203222", "btn":"002", "name":"Ezy Portfolio 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-2.html"}],
+"203223":[{"id":"203223", "btn":"002", "name":"Ezy Portfolio 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-3.html"}],
+"203224":[{"id":"203224", "btn":"002", "name":"Ezy Portfolio 4", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-4.html"}],
+"203225":[{"id":"203225", "btn":"002", "name":"Ezy Portfolio 5", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-5.html"}],
+"203226":[{"id":"203226", "btn":"002", "name":"Ezy Portfolio 6", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-6.html"}],
+"203227":[{"id":"203227", "btn":"002", "name":"Ezy Portfolio 7", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/portfolio-7.html"}],
+"203228":[{"id":"203228", "btn":"002", "name":"Ezy One Page 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/one-page-1.html"}],
+"203229":[{"id":"203229", "btn":"002", "name":"Ezy Shop 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/shop-1.html"}],
+"203230":[{"id":"203230", "btn":"002", "name":"Ezy Shop 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/shop-2.html"}],
+"203231":[{"id":"203231", "btn":"002", "name":"Ezy Shop 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/shop-3.html"}],
+"203232":[{"id":"203232", "btn":"002", "name":"Ezy Coming Soon 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/coming-soon-1.html"}],
+"203233":[{"id":"203233", "btn":"002", "name":"Ezy Coming Soon 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/coming-soon-2.html"}],
+"203234":[{"id":"203234", "btn":"002", "name":"Ezy Architecture", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-architecture.html"}],
+"203235":[{"id":"203235", "btn":"002", "name":"Ezy Business Consulting", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-business-consulting.html"}],
+"203236":[{"id":"203236", "btn":"002", "name":"Ezy Construction", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-construction.html"}],
+"203237":[{"id":"203237", "btn":"002", "name":"Ezy Gym", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-gym.html"}],
+"203238":[{"id":"203238", "btn":"002", "name":"Ezy Medical", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-medical.html"}],
+"203239":[{"id":"203239", "btn":"002", "name":"Ezy Restaurant", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/ezy/1.1.0/demo-restaurant.html"}],
+
+
+};
+
+setTimeout(function () {var gdfd=articles101[_href2];console.log(articles101[_href2]);console.log(_href2);
+if(gdfd==undefined){
+$(".iframe").html('<h2 style="padding-top: 50px;color: #1a1a1a;text-align: center;">Сайт с ID "'+_href2+'" не найден!</h2><br><a style="display: block;padding-top: 2px;text-decoration: underline;color: #5733e8;text-align: center;" href="../../index.html">Вернуться на главную ></a>');
+$('.demo_h1').html('NaN');
+}
+else{
+$.each(articles101,function(key,data) {$.each(data, function(index,value) {var id5343=value['id'];
+if(id5343==_href2){
+console.log('Запись с ключом "'+_href2+'" получина из массива!');console.log(id5343);
+$(".iframe").html(`<iframe src="${value['url']}" frameborder="0" rel="nofollow"></iframe>`);$(".demo_h1").append(`<span> ${value['id']}</span>
+	<br><b style="text-align: center;">${value['name']}</b>`);
+var eee55 = 'ID '+_href2;$(".form55 input[name=id55]").attr("value", eee55);$(".form66 input[name=id66]").attr("value", eee55);console.log('eee55 = '+eee55);
+} 
+else if (_href2==''){$(".iframe").html('<h2 style="padding-top: 50px;color: #1a1a1a;text-align: center;">Выберите сайт!</h2><br><a style="display: block;padding-top: 2px;text-decoration: underline;color: #5733e8;text-align: center;" href="../../index.html">Вернуться на главную ></a>');$('.demo_h1').html('NaN');}
+});});
+}
+}, 300);
---
+++ site-templates/demo/porto.js
@@ -0,0 +1,102 @@
+var _href2 = window.location.hash.slice(1);
+// var arr = new Map([
+//   ['101000', 'http://avehtml.liquid-themes.com/index.html'], 
+
+// ]);
+
+var articles101 = {
+"203101":[{"id":"203101", "btn":"002", "name":"Porto Classic", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-classic.html"}],
+"203102":[{"id":"203102", "btn":"002", "name":"Porto One Page", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-one-page.html"}],
+"203103":[{"id":"203103", "btn":"002", "name":"Porto Corporate 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate.html"}],
+"203104":[{"id":"203104", "btn":"002", "name":"Porto Corporate 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-2.html"}],
+"203105":[{"id":"203105", "btn":"002", "name":"Porto Corporate 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-3.html"}],
+"203106":[{"id":"203106", "btn":"002", "name":"Porto Corporate 4", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-4.html"}],
+"203107":[{"id":"203107", "btn":"002", "name":"Porto Corporate 5", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-5.html"}],
+"203108":[{"id":"203108", "btn":"002", "name":"Porto Corporate 6", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-6.html"}],
+"203109":[{"id":"203109", "btn":"002", "name":"Porto Corporate 7", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-7.html"}],
+"203110":[{"id":"203110", "btn":"002", "name":"Porto Corporate 8", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-8.html"}],
+"203111":[{"id":"203111", "btn":"002", "name":"Porto Corporate 9", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-9.html"}],
+"203112":[{"id":"203112", "btn":"002", "name":"Porto Corporate 10", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-10.html"}],
+"203113":[{"id":"203113", "btn":"002", "name":"Porto Corporate 11", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-11.html"}],
+"203114":[{"id":"203114", "btn":"002", "name":"Porto Corporate 12", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-12.html"}],
+"203115":[{"id":"203115", "btn":"002", "name":"Porto Corporate 13", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-13.html"}],
+"203116":[{"id":"203116", "btn":"002", "name":"Porto Corporate 14", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-14.html"}],
+"203117":[{"id":"203117", "btn":"002", "name":"Porto Corporate 15", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-15.html"}],
+"203118":[{"id":"203118", "btn":"002", "name":"Porto Corporate 16", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-16.html"}],
+"203119":[{"id":"203119", "btn":"002", "name":"Porto Corporate 17", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-17.html"}],
+"203120":[{"id":"203120", "btn":"002", "name":"Porto Corporate 18", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-18.html"}],
+"203121":[{"id":"203121", "btn":"002", "name":"Porto Corporate 19", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-19.html"}],
+"203122":[{"id":"203122", "btn":"002", "name":"Porto Corporate 20", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-20.html"}],
+"203123":[{"id":"203123", "btn":"002", "name":"Porto Portfolio 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-portfolio.html"}],
+"203124":[{"id":"203124", "btn":"002", "name":"Porto Portfolio 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-portfolio-2.html"}],
+"203125":[{"id":"203125", "btn":"002", "name":"Porto Portfolio 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-portfolio-3.html"}],
+"203126":[{"id":"203126", "btn":"002", "name":"Porto Portfolio 4", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-portfolio-4.html"}],
+"203127":[{"id":"203127", "btn":"002", "name":"Porto Portfolio 5", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-portfolio-5.html"}],
+"203128":[{"id":"203128", "btn":"002", "name":"Porto Blog 1", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-blog.html"}],
+"203129":[{"id":"203129", "btn":"002", "name":"Porto Blog 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-blog-2.html"}],
+"203130":[{"id":"203130", "btn":"002", "name":"Porto Blog 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-blog-3.html"}],
+"203131":[{"id":"203131", "btn":"002", "name":"Porto Blog 4", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-blog-4.html"}],
+"203132":[{"id":"203132", "btn":"002", "name":"Porto Blog 5", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-blog-5.html"}],
+"203133":[{"id":"203133", "btn":"002", "name":"Porto Hosting", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-corporate-hosting.html"}],
+"203134":[{"id":"203134", "btn":"002", "name":"Porto Classic Color", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-classic-color.html"}],
+"203135":[{"id":"203135", "btn":"002", "name":"Porto Classic Light", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-classic-light.html"}],
+"203136":[{"id":"203136", "btn":"002", "name":"Porto Classic Video", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-classic-video.html"}],
+"203137":[{"id":"203137", "btn":"002", "name":"Porto Classic Video Light", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/index-classic-video-light.html"}],
+"203138":[{"id":"203138", "btn":"002", "name":"Porto Construction", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-construction.html"}],
+"203139":[{"id":"203139", "btn":"002", "name":"Porto Construction 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-construction-2.html"}],
+"203140":[{"id":"203140", "btn":"002", "name":"Porto Business Consulting", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-business-consulting.html"}],
+"203141":[{"id":"203141", "btn":"002", "name":"Porto Business Consulting 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-business-consulting-2.html"}],
+"203142":[{"id":"203142", "btn":"002", "name":"Porto Medical", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-medical.html"}],
+"203143":[{"id":"203143", "btn":"002", "name":"Porto Medical 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-medical-2.html"}],
+"203144":[{"id":"203144", "btn":"002", "name":"Porto Finance", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-finance.html"}],
+"203145":[{"id":"203145", "btn":"002", "name":"Porto Insurance", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-insurance.html"}],
+"203146":[{"id":"203146", "btn":"002", "name":"Porto SEO", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-seo.html"}],
+"203147":[{"id":"203147", "btn":"002", "name":"Porto SEO 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-seo-2.html"}],
+"203148":[{"id":"203148", "btn":"002", "name":"Porto IT Services", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-it-services.html"}],
+"203149":[{"id":"203149", "btn":"002", "name":"Porto Product Landing", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-product-landing.html"}],
+"203150":[{"id":"203150", "btn":"002", "name":"Porto Coffee Shop", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-coffee.html"}],
+"203151":[{"id":"203151", "btn":"002", "name":"Porto SASS", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-sass.html"}],
+"203152":[{"id":"203152", "btn":"002", "name":"Porto Architecture & Interior Design", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-architecture-interior.html"}],
+"203153":[{"id":"203153", "btn":"002", "name":"Porto Barber Shop", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-barber.html"}],
+"203154":[{"id":"203154", "btn":"002", "name":"Porto Education", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-education.html"}],
+"203155":[{"id":"203155", "btn":"002", "name":"Porto Digital Agency", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-digital-agency.html"}],
+"203156":[{"id":"203156", "btn":"002", "name":"Porto Digital Agency 2 (Light)", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-digital-agency-2.html"}],
+"203157":[{"id":"203157", "btn":"002", "name":"Porto Digital Agency 2 (Dark)", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-digital-agency-2-dark.html"}],
+"203158":[{"id":"203158", "btn":"002", "name":"Porto Law Firm", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-law-firm.html"}],
+"203159":[{"id":"203159", "btn":"002", "name":"Porto App Landing", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-app-landing.html"}],
+"203160":[{"id":"203160", "btn":"002", "name":"Porto CV - Portfolio - Resume", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-resume.html"}],
+"203161":[{"id":"203161", "btn":"002", "name":"Porto Hotel", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-hotel.html"}],
+"203162":[{"id":"203162", "btn":"002", "name":"Porto Real Estate", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-real-estate.html"}],
+"203163":[{"id":"203163", "btn":"002", "name":"Porto Event", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-event.html"}],
+"203164":[{"id":"203164", "btn":"002", "name":"Porto Restaurant", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-restaurant.html"}],
+"203165":[{"id":"203165", "btn":"002", "name":"Porto Gym", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-gym.html"}],
+"203166":[{"id":"203166", "btn":"002", "name":"Porto Band", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-band.html"}],
+"203167":[{"id":"203167", "btn":"002", "name":"Porto One Page Agency", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-one-page-agency.html"}],
+"203168":[{"id":"203168", "btn":"002", "name":"Porto Photography", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-photography.html"}],
+"203169":[{"id":"203169", "btn":"002", "name":"Porto Photography 2", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-photography-2.html"}],
+"203170":[{"id":"203170", "btn":"002", "name":"Porto Photography 3", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-photography-3.html"}],
+"203171":[{"id":"203171", "btn":"002", "name":"Porto Church", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-church.html"}],
+"203172":[{"id":"203172", "btn":"002", "name":"Porto Wedding", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/demo-wedding.html"}],
+"203173":[{"id":"203173", "btn":"002", "name":"Porto Boxed", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/feature-layout-boxed.html"}],
+"203174":[{"id":"203174", "btn":"002", "name":"Porto RTL", "theme":"l1 l2 l3 l4 l5 l6 l7 l8 l9", "url":"https://preview.oklerthemes.com/porto/8.0.0/feature-layout-rtl.html"}],
+
+
+};
+
+setTimeout(function () {var gdfd=articles101[_href2];console.log(articles101[_href2]);console.log(_href2);
+if(gdfd==undefined){
+$(".iframe").html('<h2 style="padding-top: 50px;color: #1a1a1a;text-align: center;">Сайт с ID "'+_href2+'" не найден!</h2><br><a style="display: block;padding-top: 2px;text-decoration: underline;color: #5733e8;text-align: center;" href="../../index.html">Вернуться на главную ></a>');
+$('.demo_h1').html('NaN');
+}
+else{
+$.each(articles101,function(key,data) {$.each(data, function(index,value) {var id5343=value['id'];
+if(id5343==_href2){
+console.log('Запись с ключом "'+_href2+'" получина из массива!');console.log(id5343);
+$(".iframe").html(`<iframe src="${value['url']}" frameborder="0" rel="nofollow"></iframe>`);$(".demo_h1").append(`<span> ${value['id']}</span>
+	<br><b style="text-align: center;">${value['name']}</b>`);
+var eee55 = 'ID '+_href2;$(".form55 input[name=id55]").attr("value", eee55);$(".form66 input[name=id66]").attr("value", eee55);console.log('eee55 = '+eee55);
+} 
+else if (_href2==''){$(".iframe").html('<h2 style="padding-top: 50px;color: #1a1a1a;text-align: center;">Выберите сайт!</h2><br><a style="display: block;padding-top: 2px;text-decoration: underline;color: #5733e8;text-align: center;" href="../../index.html">Вернуться на главную ></a>');$('.demo_h1').html('NaN');}
+});});
+}
+}, 300);
