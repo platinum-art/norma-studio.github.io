f22c012dc51e603dfe1bc772e520402e303f5438
norma-studio <65451081+norma-studio@users.noreply.github.com>
2020-06-17 15:20:37 +0400

    Add files via upload

--- site-templates/demo/mnu.js
+++ site-templates/demo/mnu.js
@@ -3,7 +3,7 @@ $('.wr_mnu>.mnu').append(`
 <span><a class="a_mnu0" href="../../index.html#ex1" title="Разработка сайтов от Norma Studio">Главная</a></span>
 <span><a class="a_mnu3" href="../../index.html#ex4" title="Разработка сайтов от Norma Studio">Дизайн сайтов</a></span>
 <span><a class="a_mnu16" href="../portfolio.html" target="_blank" title="Портфолио по разработке сайтов от Norma Studio">Портфолио</a></span>
-<span><a class="a_mnu5" href="../../index.html#ex2" title="Разработка сайтов от Norma Studio">Услуги</a></span>
+<span><a class="a_mnu5" href="../../index.html#ex23" title="Разработка сайтов от Norma Studio">Тарифы</a></span>
 <span><a class="a_mnu10" href="../testimonials.html" target="blank" title="Отзывы клиентов | Разработка сайтов от Norma Studio">Отзывы</a></span>
 <span><a class="a_mnu6" href="../../index.html#ex6" title="Разработка сайтов от Norma Studio">Контакты</a></span>
 <span><a data-remodal-target="modal5" title="Разработка сайтов от Norma Studio" style="color: #5733e8;cursor: pointer;border-bottom: 1px solid #5733e8;">Заказать</a></span>
